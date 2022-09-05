a1ec3d461351c5b3a3766aa73bda0bfae66c0720
Fabio <fabio.cattolico@gmx.de>
2022-09-04 19:24:41 -0500

    Implemented DateRangePicker and set up date selection

--- Components/Header.js
+++ Components/Header.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import Image from 'next/image';
 import {SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon, UserIcon} from "@heroicons/react/solid"
+import {useState} from "react"
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import { DateRangePicker } from 'react-date-range';
+
 
 function Header() {
+
+    const [searchInput, setSearchInput] = useState("");
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const selectionRange = {
+        startDate: startDate,
+        endDate: endDate,
+        key: "selection"
+    };
+
+    const handleSelect = (ranges) => {
+        setStartDate(ranges.selection.startDate); 
+        setEndDate(ranges.selection.endDate);
+        };
+    
+
   return (
     <header className='sticky top-0 z-50 grid grid-cols-3 bg-white my-0 shadow-md p-5 w-full md:px-10'>
         <div className="relative flex items-center h-10 cursor-pointer my-auto">
@@ -15,7 +36,8 @@ function Header() {
         </div>
         
         <div className='flex item-center md:border-2 rounded-full py-2 md:shadow-sm'>
-            <input type="text" placeholder='Explore' className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400' />
+            <input value={searchInput} onChange={(e) => setSearchInput(e.target.value)}
+            type="text" placeholder='Explore' className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400' />
             <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2' />
         </div>
        
@@ -27,6 +49,17 @@ function Header() {
                 <UserCircleIcon className='h-6 cursor-pointer' />
             </div>
         </div>
+
+        {searchInput && (
+            <div>
+                <DateRangePicker 
+                    ranges={[selectionRange]}
+                    minDate={new Date()}
+                    rangeColors={["#FD5B61"]}
+                    onChange={handleSelect}
+                />
+            </div>
+        )}
     </header>
   )
 }
